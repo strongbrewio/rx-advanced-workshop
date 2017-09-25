dd87bffe9079ee64f28e4a8904defb0a82c61ddc
KwintenP <kwinten.pisman@gmail.com>
2017-09-25 19:08:20 +0200

    moved some stuff around

---
+++ giphy-app/src/giphy/guards/is-logged-in.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import {AuthenticationService} from '../services/authentication.service';
+
+@Injectable()
+export class IsLoggedInGuard implements CanActivate {
+
+  constructor(private authenticationService: AuthenticationService) {
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authenticationService.authenticate();
+  }
+}
