ecc716223a982d30c9570b560cd77a31bccc4bec
KwintenP <kwinten.pisman@gmail.com>
2017-09-25 21:01:17 +0200

    added test begin

--- giphy-app/src/giphy/containers/index-page/index-page.container.spec.ts
+++ giphy-app/src/giphy/containers/index-page/index-page.container.spec.ts
@@ -2,8 +2,8 @@ import {IndexPageContainer} from './index-page.container';
 import {AuthenticationService} from '../../services/authentication.service';
 import {GiphyService} from '../../services/giphy.service';
 import {ActivatedRoute} from '@angular/router';
-import createSpyObj = jasmine.createSpyObj;
 import {marbles} from 'rxjs-marbles';
+import createSpyObj = jasmine.createSpyObj;
 describe('container: IndexPageContainer', () => {
   let container: IndexPageContainer;
   let authenticationServiceMock: AuthenticationService;
@@ -22,8 +22,32 @@ describe('container: IndexPageContainer', () => {
   });
 
   describe('on ngAfterViewInit', () => {
-    it('should set up the streams for fetching the data with an infinite scroll', marbles((m)) => {
+    it('should set up the streams for fetching the data with an infinite scroll', marbles((m) => {
+      // @formatter:off
+      const values = {
 
-    });
+      };
+      const scroll$ =       m.cold('-------x------x');
+      // @formatter:on
+
+
+      const giphyOverviewElementMock = {
+        nativeElement: {
+          addEventListener: (eventType, listener) => {
+
+          },
+          removeEventListener: () => void 0,
+          dispatchEvent: () => void 0,
+          get scrollTop() {
+            const data = [250, 500];
+            return data.splice(0, 1);
+          }
+        }
+      };
+
+      scroll$.subscribe(val => {
+          console.log('val');
+      });
+    }));
   });
 });
